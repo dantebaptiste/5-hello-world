1b3955f91a2c3070ae2de1f6628c65a1b169ffae
Dante Baptiste <dantebaptiste@REVI024.local>
2021-06-09 14:27:16 -0500

    added the new sketch.py and changes to the config file

--- sketch.py
+++ sketch.py
@@ -1,1 +1,14 @@
-print("Hello World")
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    if y == 0:
+        raise ValueError("Can not divide by zero!")
+    return x / y
+
